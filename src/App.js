0e9f37c29882dcd4f89b346e3a596d41aaae822a
ArifBabayev05 <arifrb@code.edu.az>
2022-08-02 13:35:55 +0400

    Facebook-Clone

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Header from './Header';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Facebook Clone</h1>
+      <Header/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCp3urlUjIRdTBXq5B5iDSmu0buzKoMIdU",
+    authDomain: "facebook-ca673.firebaseapp.com",
+    projectId: "facebook-ca673",
+    storageBucket: "facebook-ca673.appspot.com",
+    messagingSenderId: "458305566423",
+    appId: "1:458305566423:web:f0ab4655e0931084b8a292",
+    measurementId: "G-B0E59LH122"
+  };
